fc5afea8301a7db0223def0c21ea13e72ee69909
Steven Bull <stevenwbulljr@gmail.com>
2019-11-26 20:13:33 +0000

    add a little more logic to static home page

--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -4,19 +4,21 @@ import './HomePage.css';
 export default class HomePage extends Component {
 
   renderArticlesToPage = () => {
-    return 'Articles will be rendered here'
+    return <div>Articles will be rendered here</div>
   }
 
   render() {
     return (
       <section>
-        <div>
+        <div className='articleQueryContainer'>
           <span>Top News</span>
           <span>Technology</span>
           <span>Sports</span>
           <span>Business</span>
         </div>
-        <div>
+        <div className='articleContainer'>
+          {this.renderArticlesToPage()}
+          {this.renderArticlesToPage()}
           {this.renderArticlesToPage()}
         </div>
       </section>
