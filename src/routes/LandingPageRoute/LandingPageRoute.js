f0225d075314d82edd887b83de4343264ff60b11
Steven Bull <stevenwbulljr@gmail.com>
2019-11-26 16:47:35 +0000

    finish static landing page

--- src/routes/LandingPageRoute/LandingPageRoute.js
+++ src/routes/LandingPageRoute/LandingPageRoute.js
@@ -3,7 +3,36 @@ import React, { Component } from 'react';
 export default class LandingPageRoute extends Component {
   render() {
     return (
-      <h1>Hello world!</h1>
+      <>
+        <section>
+          <div>
+            <h2>Welcome to Bread Crumbs!</h2>
+            <div>
+              <button>Join Today!</button>
+            </div>
+          </div>
+          <div>
+            <p>Possibly an image here? and off center with h2</p>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h4>Section 1</h4>
+            <p>This can be a flexboxed section</p>
+          </div>
+          <div>
+            <h4>Section 2</h4>
+            <p>In the middle of the page</p>
+          </div>
+          <div>
+            <h4>Section 3</h4>
+            <p>That shows some great features</p>
+          </div>
+        </section>
+        <section>
+          <p>Maybe some bottom page stuffs? Idk</p>
+        </section>
+      </>
     )
   }
 }
