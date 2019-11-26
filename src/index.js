215b0dc9c9fcbed20d0b695762d52ae86c542ff0
Steven Bull <stevenwbulljr@gmail.com>
2019-11-26 17:34:59 +0000

    format UserContext to provide on index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App/App';
+import UserProvider from './contexts/UserContext';
 import 'normalize.css';
 import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </BrowserRouter>, 
   document.getElementById('root')
 );
