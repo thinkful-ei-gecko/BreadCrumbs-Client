4b655959713209ccccddcd7baecb2a19cdfe8359
Steven Bull <stevenwbulljr@gmail.com>
2019-11-26 17:45:43 +0000

    finish footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
-import {Route,Switch} from 'react-router-dom'
-import Header from '../Header/Header'
+import {Route,Switch} from 'react-router-dom';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import LandingPageRoute from '../../routes/LandingPageRoute/LandingPageRoute';
 import LoginPage from '../../routes/LoginPage/LoginPage';
-import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage'
-import PublicRoute from '../../routes/PublicRoute/PublicRoute'
-import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage'
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage';
+import PublicRoute from '../../routes/PublicRoute/PublicRoute';
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage';
 
 export default class App extends Component {
  
   render() {
     return (
-      <div>
-          <header>
+      <>
+        <header>
           <Header />
-          </header>
-          <main>
+        </header>
+        <main>
           <Switch>
-          <Route exact path={'/'} component={LandingPageRoute} />
-          <PublicRoute path={'/login'} component={LoginPage}/>
-          <PublicRoute path={'/register'} component={RegistrationPage}/>
-          <Route component={NotFoundPage} />
-        </Switch> 
+            <Route exact path={'/'} component={LandingPageRoute} />
+            <PublicRoute path={'/login'} component={LoginPage}/>
+            <PublicRoute path={'/register'} component={RegistrationPage}/>
+            <Route component={NotFoundPage} />
+          </Switch>
+        <footer>
+          <Footer />
+        </footer>
         </main>
-      </div>
+      </>
     )
   }
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  render() {
+    return (
+      <section>
+        <div>
+          <p>Copyright | Bread Crumbs | 2019</p>
+        </div>
+      </section>
+    )
+  }
+}
