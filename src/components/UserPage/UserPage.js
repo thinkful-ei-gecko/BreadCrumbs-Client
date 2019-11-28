bb60919f731bf785bfb7a8778acdce17f83789c1
Badri-narayan <badritulsiram@gmail.com>
2019-11-28 11:51:24 -0800

    some styling to the settings page

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
+import './UserPage.css'
 
 export default class UserPage extends Component {
   render() {
     return (
-      <div>
+      <section className='Settings'>
         <h2>UserName</h2>
-        <Link to='/settings'>Settings</Link>
-        <h3>Account Settings</h3>
-        <p>username</p>
-        <button>edit</button>
-        <p>Password</p>
-        <button>edit</button>
-      </div>
+        {/* <Link to='/settings'>Settings</Link> */}
+        <h3 id='accSetting'>
+          <img className='gearThing' src='https://image.flaticon.com/icons/svg/0/525.svg' alt='settings' /> 
+          <div className='accSettings'> Account Settings </div>
+        </h3>
+        <div className='settings'>
+          <p>username</p>
+          <button className='settingBtn'>edit</button>
+        </div>
+        <div className='settings'>
+          <p>Password</p>
+          <button className='settingBtn'> edit</button>
+        </div>
+      </section>
     )
   }
 }
