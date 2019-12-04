3cf4e6d9cb8ff076c5043c0913e826ef1b71b141
Steven Bull <stevenwbulljr@gmail.com>
2019-12-04 18:00:59 +0000

    refactor code to follow DRY principles

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   API_ENDPOINT: 'https://breadcrumbs-ei34.herokuapp.com/api',
   TOKEN_KEY: process.env.BREAD_CRUMBS_AUTH_TOKEN || 'bread-crumbs-auth-token',
-  API_KEY:process.env.API
+  REACT_APP_API_KEY: process.env.REACT_APP_API_KEY
 }
 
 // export default {
--- src/routes/HomePage/HomePage.js
+++ src/routes/HomePage/HomePage.js
@@ -7,83 +7,26 @@ import './HomePage.css';
 
 export default class HomePage extends Component {
   static contextType = ArticlesContext;
-handleTopNews(){
-  
-  fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=948441d54c94474081783b97b5cc1da3',{
-    headers: {
-      'authorization': `bearer ${config.API_KEY}`, 
-    }
-  })
-  .then(res=>res.json())     
-  .then(data => {
-        
-         this.context.setArticlesList(data.articles);
-      })
-}
-
-handleBuisness(){
-  fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=948441d54c94474081783b97b5cc1da3')
-  .then(res=>res.json())     
-  .then(data1 => {
-        
-         this.context.setArticlesList(data1.articles);
-      })
-}
-handleEntertainment(){
-  fetch(' https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=948441d54c94474081783b97b5cc1da3')
-  .then(res=>res.json())     
-  .then(data => {
-        console.log(data)
-         this.context.setArticlesList(data.articles);
-      })
- 
-}
-
-handleHealth(){
-  fetch('https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=948441d54c94474081783b97b5cc1da3')
-  .then(res=>res.json())     
-  .then(data => {
-        console.log(data)
-         this.context.setArticlesList(data.articles);
-      })
-}
-
-handleScience(){
-  fetch('https://newsapi.org/v2/top-headlines?country=us&category=science&apiKey=948441d54c94474081783b97b5cc1da3')
-  .then(res=>res.json())     
-  .then(data => {
-        console.log(data)
-         this.context.setArticlesList(data.articles);
-      })
-}
-
-handleSports(){
-  fetch('https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=948441d54c94474081783b97b5cc1da3')
-  .then(res=>res.json())     
-  .then(data => {
-        console.log(data)
-         this.context.setArticlesList(data.articles);
-      })
-}
-
-handleTechnology(){
-  fetch('https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=948441d54c94474081783b97b5cc1da3')
-  .then(res=>res.json())     
-  .then(data => {
-        console.log(data)
-         this.context.setArticlesList(data.articles);
-      })
-}
 
   componentDidMount() {
     this.handleTopNews()
-    // this.handleBuisness()
-    // this.handleEntertainment()
-    // this.handleHealth()
-    // this.handleScience()
-    // this.handleSports()
-    // this.handleTechnology()
-   }
+  }
+  
+  handleTopNews(){
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${config.REACT_APP_API_KEY}`)
+    .then(res=>res.json())     
+    .then(data => { 
+          this.context.setArticlesList(data.articles);
+        })
+  }
+
+  handleNewNews(subject){
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${subject}&apiKey=${config.REACT_APP_API_KEY}`)
+    .then(res=>res.json())     
+    .then(data => { 
+          this.context.setArticlesList(data.articles);
+        })
+  }
 
   renderArticlesToPage() {
      //const { articlesList = [] } = this.context;
@@ -110,17 +53,16 @@ handleTechnology(){
       <section>
         <div className='articleQueryContainer'>
           <ul>
-          <li onClick={()=>this.handleTopNews()}>Top News</li>
-          <li onClick={()=>this.handleBuisness()}>Buisness</li>
-          <li onClick={()=>this.handleHealth()}>Health</li>
-          <li onClick={()=>this.handleScience()}>Science</li>
-          <li onClick={()=>this.handleSports()}>Sports</li>
-          <li onClick={()=>this.handleTechnology()}>Technology</li>
+          <li onClick={() => this.handleTopNews()}>Top News</li>
+          <li onClick={() => this.handleNewNews('business')}>Business</li>
+          <li onClick={() => this.handleNewNews('health')}>Health</li>
+          <li onClick={() => this.handleNewNews('science')}>Science</li>
+          <li onClick={() => this.handleNewNews('sports')}>Sports</li>
+          <li onClick={() => this.handleNewNews('technology')}>Technology</li>
           </ul>
         </div>
         <div className='articleContainer'>
           {this.renderArticlesToPage()}
-          
         </div>
       </section>
     )
