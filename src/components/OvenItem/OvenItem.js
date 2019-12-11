c2a5e738c844b257c842ec87fd01719a85d2f66b
Steven Bull <stevenwbulljr@gmail.com>
2019-12-11 18:12:05 +0000

    start on user updating

--- src/components/OvenItem/OvenItem.js
+++ src/components/OvenItem/OvenItem.js
@@ -1,76 +1,129 @@
-import React, { Component } from 'react';
-import './OvenItem.css';
-import news from '../NewsItem/images/news.jpg';
-import Comments from '../../components/Comments/Comments';
-import ArticlesApiService from '../../services/articles-api-service';
-import UserAndArticlesContext from '../../contexts/UserAndArticlesContext';
+import React, { Component } from "react";
+import "./OvenItem.css";
+import news from "../NewsItem/images/news.jpg";
+import Comments from "../../components/Comments/Comments";
+import ArticlesApiService from "../../services/articles-api-service";
+import UserAndArticlesContext from "../../contexts/UserAndArticlesContext";
 
 export default class NewsItem extends Component {
   state = {
     articleId: null
-  }
+  };
+
   static contextType = UserAndArticlesContext;
-  
-  handleUpVote=(article_id)=>{
-    console.log(this.context.user)
-    const user_id = this.context.user.id
-    const vote_type = true
-    ArticlesApiService.updateVote(article_id,user_id,vote_type)
 
-  }
+  handleUpVote = article_id => {
+    console.log(this.context.user);
+    const user_id = this.context.user.id;
+    const vote_type = true;
+    ArticlesApiService.updateVote(article_id, user_id, vote_type);
+  };
 
-  handleDownVote=(article_id)=>{
-    const user_id = this.context.user.id
-    const vote_type = false
-    ArticlesApiService.updateVote(article_id,user_id,vote_type)
-  }
-  handleSavedArticle=(article_id)=>{
-    console.log('savedarticle')
-    const user_id = this.context.user.id
-    ArticlesApiService.postSavedArticle(article_id,user_id)
-  }
+  handleDownVote = article_id => {
+    const user_id = this.context.user.id;
+    const vote_type = false;
+    ArticlesApiService.updateVote(article_id, user_id, vote_type);
+  };
+  handleSavedArticle = article_id => {
+    console.log("savedarticle");
+    const user_id = this.context.user.id;
+    ArticlesApiService.postSavedArticle(article_id, user_id);
+  };
 
-  handleRenderComments = async(article_id) => {
-    const data = await ArticlesApiService.getComments(article_id)
-    this.context.setArticleComments(data)
-    this.state.articleId === article_id 
+  handleRenderComments = async article_id => {
+    const data = await ArticlesApiService.getComments(article_id);
+    this.context.setArticleComments(data);
+    this.state.articleId === article_id
       ? this.setState({ articleId: null })
-      : this.setState({ articleId: article_id })
+      : this.setState({ articleId: article_id });
+  };
+
+  render() {
+    const { articleId } = this.state;
+    const {
+      article_id,
+      author,
+      content,
+      source,
+      description,
+      title,
+      url,
+      url_to_image,
+      vote_count
+    } = this.props;
+    
+    return (
+      <li className="listItem">
+        <div className="score">
+          <button className="OvenItemBtn">
+            <img
+              src="https://image.flaticon.com/icons/svg/2224/2224092.svg"
+              alt="fresh bread"
+              className="fresh"
+              onClick={() => this.handleUpVote(article_id)}
+            />
+          </button>
+          <p>{vote_count}</p>
+          <button className="OvenItemBtn">
+            {" "}
+            <img
+              src="https://image.flaticon.com/icons/svg/2224/2224092.svg"
+              alt="not-fresh"
+              className="not-fresh"
+              onClick={() => this.handleDownVote(article_id)}
+            />{" "}
+          </button>
+          <button className="OvenItemBtn">
+            <img
+              src="https://image.flaticon.com/icons/svg/148/148836.svg"
+              alt="heart"
+              className="comments"
+              onClick={() => this.handleSavedArticle(article_id)}
+            />
+          </button>
+          <button className="OvenItemBtn">
+            <img
+              src="https://image.flaticon.com/icons/svg/134/134914.svg"
+              alt="comments"
+              className="comments"
+              onClick={() => this.handleRenderComments(article_id)}
+            />
+          </button>
+        </div>
+
+        <div className="item">
+          {url_to_image === null ? (
+            <img src={news} alt="img" className="img" />
+          ) : (
+            <img src={url_to_image} alt="img" className="img" />
+          )}
+          <div className="article-section">
+            <h3> {title}</h3>
+            {author != null ? <p>By: {author}</p> : null}
+            <p>Source: {source}</p>
+            <p> {description}</p>
+            <a
+              href={url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="openLinkBtn"
+            >
+              View Article
+              <img
+                className="open-link"
+                src="https://image.flaticon.com/icons/svg/1/1424.svg"
+                alt="open link"
+              />{" "}
+            </a>
+          </div>
+        </div>
+
+        <div className="comments-container">
+          {articleId === article_id ? (
+            <Comments articleID={article_id} />
+          ) : null}
+        </div>
+      </li>
+    );
   }
-
-    render () {
-      const { articleId } = this.state;
-      const { article_id, author, content, source, description, title, url, url_to_image, vote_count} = this.props
-        return (
-        <li className ='listItem'>
-          <div className='score'>
-             <button className='OvenItemBtn'><img src='https://image.flaticon.com/icons/svg/2224/2224092.svg' alt='fresh bread' className='fresh' onClick={()=>this.handleUpVote(article_id)} /></button>  
-              <p>{vote_count}</p>
-             <button className='OvenItemBtn'> <img src='https://image.flaticon.com/icons/svg/2224/2224092.svg' alt='not-fresh' className='not-fresh' onClick={()=>this.handleDownVote(article_id)} /> </button> 
-            {/* <Link to={{pathname:'/comments', params:{article_id:article_id}}}>
-             <button className='OvenItemBtn'> <img src='https://image.flaticon.com/icons/svg/134/134914.svg' alt='comments' className='comments' /> </button>
-             </Link> */}
-             <button className='OvenItemBtn'><img src='https://image.flaticon.com/icons/svg/148/148836.svg' alt='heart' className='comments' onClick={() => this.handleSavedArticle(article_id)} /></button>
-             <button className='OvenItemBtn'><img src='https://image.flaticon.com/icons/svg/134/134914.svg' alt='comments' className='comments' onClick={() => this.handleRenderComments(article_id)}/></button>
-            </div> 
-            
-          <div className='item'>
-          { url_to_image === null ?
-           <img src={news} alt='img' className='img' />: 
-           <img src={url_to_image} alt='img' className='img' /> }
-            <div className='article-section'>
-          <h3> {title}</h3>
-          {author != null ? <p>By: {author}</p> : null }
-          <p>Source: {source}</p>
-          <p> {description}</p>
-            <a href={url} target='_blank'rel='noopener noreferrer' className='openLinkBtn'>View Article<img className = 'open-link' src='https://image.flaticon.com/icons/svg/1/1424.svg' alt='open link' /> </a>
-          </div>
-          </div>
-
-          <div className='comments-container'>
-            {(articleId === article_id) ? <Comments articleID={article_id}/> : null}
-          </div>
-        </li>
-        )
-    }
 }
--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -2,7 +2,6 @@ import config from "../config";
 
 
 const AuthApiService = {
-
   postLogin({ username, password }) {
     return fetch(`${config.API_ENDPOINT}/auth/token`, {
       method: "POST",
@@ -15,46 +14,44 @@ const AuthApiService = {
     )
  
   },
-    postUser(user) {
-          return fetch(`${config.API_ENDPOINT}/user`, {
-            method: 'POST',
-            headers: {
-              'content-type': 'application/json',
-            },
-            body: JSON.stringify(user),
-          })
-            .then(res =>
-              (!res.ok)
-                ? res.json().then(e => Promise.reject(e))
-                : res.json()
-            )
-           
-        },
-
-        postChangeUsername({id,username,name,password}) {
-          return fetch(`${config.API_ENDPOINT}/user/update`, {
-            method: "PATCH",
-            headers: {
-              "content-type": "application/json"
-            },
-            body: JSON.stringify({id,username,name,password})
-          }).then(res =>
-            !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-          )
-       
-        },
-        postChangePassword({id,old_password,new_password,confirm_password}) {
-          return fetch(`${config.API_ENDPOINT}/user/update`, {
-            method: "PATCH",
-            headers: {
-              "content-type": "application/json"
-            },
-            body: JSON.stringify({id,old_password,new_password,confirm_password})
-          }).then(res =>
-            !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
-          )
-       
-        },
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/user`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+      
+  },
+  postChangeUsername({ id, username, name, password}) {
+    return fetch(`${config.API_ENDPOINT}/user/update`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({id,username,name,password})
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    )
+  },
+  postChangePassword({ id, old_password, new_password, confirm_password}) {
+    return fetch(`${config.API_ENDPOINT}/user/update/password`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({id,old_password,new_password,confirm_password})
+    }).then(res =>
+      !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
+    )
+  
+  },
 };
 
 export default AuthApiService;
