8c4c15360018a84dc3fda2b6cccfb32467818079
Steven Bull <stevenwbulljr@gmail.com>
2019-12-12 15:41:15 +0000

    add responsiveness to login/register clicking in hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -53,11 +53,11 @@ export default class Header extends Component {
   renderLoginLink() {
     return (
       <div className="menu">
-        <Link className="headerLinks" to="/register">
+        <Link className="headerLinks" to="/register" onClick={this.hideElements}>
           Register
         </Link>
         <span></span>
-        <Link className="headerLinks" to="/login">
+        <Link className="headerLinks" to="/login" onClick={this.hideElements}>
           Log in
         </Link>
       </div>
